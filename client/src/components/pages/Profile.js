d4209c3200e2ef82681b5f5ea26c7852e3e55b58
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-16 11:06:12 -0400

    added form submit and input change for profile page

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -14,7 +14,7 @@ export default class Profile extends Component {
     }
 
     handleInputChange = event => {
-        const name = evnet.target.name;
+        const name = event.target.name;
         const value = event.target.value;
         console.log(name)
         this.setState ({
@@ -31,6 +31,8 @@ export default class Profile extends Component {
             skillwanted: this.state.skillwanted,
             bio: this.state.bio,
     }
+    this.setState({firstname:'', lastname:'', city: '', zipcode: '', skillshare: '', skillwanted:'', bio: ''})
+
 }
 
     render() {
@@ -39,29 +41,29 @@ export default class Profile extends Component {
         <div>
             <div className="container">
 ​
-<h3 class="text-center mt-3">Join Us and Grow Your Skills Today</h3>
+<h3 class="text-center mt-3">It's all about them skills!!</h3>
 <form id="survey-form">
     <div class="row">
       <div class="form-group col">
         <label id="name-label" for="name">First Name</label>
-        <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Enter your first name" required />
+        <input type="text" name="firstname" value={this.state.firstname} onChange={this.handleInputChange} id="firstname" class="form-control" placeholder="Enter your first name" required />
       </div>
       <div class="form-group col">
         <label id="name-label" for="name">Last Name</label>
-        <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Enter your last name" required />
+        <input type="text" name="lastname" value={this.state.lastname}  onChange={this.handleInputChange}id="lastname" class="form-control" placeholder="Enter your last name" required />
       </div>
 ​
     </div>
     <div class="row">
       <div class="form-group col">
         <label id="city-label" for="city">City</label>
-        <input type="text" name="city" id="city" class="form-control" placeholder="Enter Your City Location" />
+        <input type="text" name="city" value={this.state.city} onChange={this.handleInputChange} id="city" class="form-control" placeholder="Enter Your City Location" />
       </div>
 ​
 ​
       <div class="form-group col">
         <label id="zipCode-label" for="email">Zip Code</label>
-        <input type="text" name="zipcode" id="zipcode" class="form-control" placeholder="Enter your Zip Code" required /> 
+        <input type="text" name="zipcode" value={this.state.zipcode} onChange={this.handleInputChange} id="zipcode" class="form-control" placeholder="Enter your Zip Code" required /> 
       </div>
 
 ​
@@ -71,7 +73,7 @@ export default class Profile extends Component {
     <div className="row">
       <div className="form-group col">
         <p>Which skill do you want to share?</p>
-        <select id="skillshare" name="skillshare" className="form-control" required>
+        <select id="skillshare" name="skillshare" value={this.state.skillshare} onChange={this.handleInputChange} className="form-control" required>
           <option disabled selected value>Select an option</option>
           <option value="JavaScript">JavaScript</option>
           <option value="Node.js">Node.js</option>
@@ -87,7 +89,7 @@ export default class Profile extends Component {
         <p>
           Which skill do you want to learn?
         </p>
-        <select id="skillwanted" name="skillwanted" className="form-control" required>
+        <select id="skillwanted" name="skillwanted" value={this.state.skillwanted} onChange={this.handleInputChange} className="form-control" required>
           <option disabled selected value>Select an option</option>
           <option value="Piano">Piano</option>
           <option value="Excel">Excel</option>
@@ -104,11 +106,11 @@ export default class Profile extends Component {
 ​
     <div className="form-group">
       <p>Bio</p>
-      <textarea id="bio" className="input-textarea form-control" name="comment" placeholder="Tell us more about you"></textarea>
+      <textarea id="bio" className="input-textarea form-control" value={this.state.bio} name="bio" onChange={this.handleInputChange}placeholder="Tell us more about you"></textarea>
     </div>
 ​
     <div className="form-group center">
-      <a href="/members" type="submit" id="submit" className="btn btn-outline-dark mr-3">
+      <a href="/members" type="submit" id="submit" onClick={this.handleFormSubmit} className="btn btn-outline-dark mr-3">
         Submit
       </a>
       <a href="/members" type="submit" className="btn btn-outline-dark">
